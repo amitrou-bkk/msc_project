592ab0b792bf7179c8ecf4022788eeecce5c990c
amitrou-bbk <amitro02@dcs.bbk.ac.uk>
2022-10-19 21:36:22 +0300

    camera capture

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from edge_layer.edge_controller import EdgeController, IngressMode
+from device_layer.CameraCapture import CameraCapture
+import threading
+import os
 
 if __name__ == '__main__':
     try:
-        controller = EdgeController(IngressMode.EdgeDataIngressMode.FileSystem)
-        controller.startListening()
+        capture = CameraCapture(os.environ["CAMERA_IP"], os.environ["CAMERA_PORT"], False, os.environ["user"], os.environ["password"])
+        image_capture_thread = threading.Thread(target = capture.Start)
+        image_capture_thread.start()
+        
+        edgeController = EdgeController(IngressMode.EdgeDataIngressMode.FileSystem)
+        edgeControllerThread = threading.Thread(target = edgeController.startListening)
+        edgeControllerThread.start()
+        
     except KeyboardInterrupt:
-        controller.stopListening()
+        image_capture_thread.join()
+        edgeControllerThread.join()
---
+++ device_layer/CameraCapture.py
@@ -0,0 +1,37 @@
+# Using Android IP Webcam video .jpg stream (tested) in Python2 OpenCV3
+import cv2
+import numpy as np
+import time
+import os
+import imageio.v3 as iio
+
+
+class CameraCapture:
+
+    def __init__(self, camera_ip, camera_port = None , isHttps = False, user = None, password = None):
+        self.url = "https://" if isHttps else "http://"
+        self.url += f"{user}:{password}@" if user is not None and password is not None else ""
+        self.url += camera_ip
+        self.url += f":{camera_port}" if camera_port is not None else ""
+        self.url += "/video"
+        self.IsCapturing = False
+       
+    def Start(self, delayInSeconds=0):
+        cap = cv2.VideoCapture(self.url)
+        frame_count = delayInSeconds
+        self.IsCapturing = True
+        while (self.IsCapturing):
+            _, frame = cap.read()
+            cv2.imshow("Frame", frame)
+            iio.imwrite(f"frame_{frame_count}.jpg", frame)
+            frame_count = frame_count + 1
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+            time.sleep(1)
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def Stop(self):
+        self.IsCapturing = False
